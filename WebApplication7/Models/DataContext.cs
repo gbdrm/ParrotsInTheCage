e30ac35b05538197401ab9e718e9e4abe59aa528
Viktor Kotsiuban <gbdrm1@gmail.com>
2017-03-08 11:39:02 +0200

    Add Identity

--- WebApplication7/Models/DataContext.cs
+++ WebApplication7/Models/DataContext.cs
@@ -1,11 +1,17 @@
 ﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace WebApplication7.Models
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Parrot> Parrots { get; set; }
 
         public DbSet<Cage> Cages { get; set; }
+        
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
     }
 }
